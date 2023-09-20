9e56e3d1f1254da71ff6ad633e4bf6466ed50ad0
David Rose <doppler@gmail.com>
2023-09-20 17:42:47 -0600

    LineWithLegend add stroke-width

--- src/lib/components/LineWithLegend.svelte
+++ src/lib/components/LineWithLegend.svelte
@@ -3,12 +3,13 @@
 
 	export let lineArray: Line[];
 	export let name: string;
+	export let strokeWidth: number = 1;
 	export let fill: string = 'yellow';
 	export let showLegend: Boolean = false;
 </script>
 
 {#each lineArray as l, i}
-	<line x1={l[0].x} y1={l[0].y} x2={l[1].x} y2={l[1].y} class={name} />
+	<line x1={l[0].x} y1={l[0].y} x2={l[1].x} y2={l[1].y} class={name} stroke-width={strokeWidth} />
 	{#if showLegend}
 		<text {fill} x={midpoint(l).x} y={midpoint(l).y}>{`${name}${i}`}</text>
 	{/if}
