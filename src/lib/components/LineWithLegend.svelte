ab996e209360fd09456abfc4474909a5b2231b36
David Rose <doppler@gmail.com>
2023-10-02 20:08:40 -0600

    add optional quarterPoint attr

--- src/lib/components/LineWithLegend.svelte
+++ src/lib/components/LineWithLegend.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-	import { midpoint } from '$lib/geometry';
+	import { midpoint, quarterPoint } from '$lib/geometry';
 
 	export let lineArray: Line[];
 	export let name: string = '';
 	export let fill: string = 'yellow';
 	export let showLegend: Boolean = false;
 	export let style = '';
+	export let useQuarterPoint = false;
 </script>
 
 {#each lineArray as l, i}
 	<line x1={l[0].x} y1={l[0].y} x2={l[1].x} y2={l[1].y} class={name} {style} />
 	{#if showLegend}
-		<text {fill} x={midpoint(l).x} y={midpoint(l).y}>{`${name}${i}`}</text>
+		<text
+			{fill}
+			x={useQuarterPoint ? quarterPoint(l).x : midpoint(l).x}
+			y={useQuarterPoint ? quarterPoint(l).y : midpoint(l).y}>{`${name}${i}`}</text
+		>
 	{/if}
 {/each}
 
