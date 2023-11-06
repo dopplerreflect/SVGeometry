3d0b1fd0267ab6ec9a6ba5f13001fe74ea322092
David Rose <doppler@gmail.com>
2023-11-06 07:47:07 -0600

    colorize quasicrystal

--- src/routes/drawings/quasicrystal/+page.svelte
+++ src/routes/drawings/quasicrystal/+page.svelte
@@ -10,12 +10,12 @@
 	import Background from '$lib/components/Background.svelte';
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
 	import LineWithLegend from '$lib/components/LineWithLegend.svelte';
-	import { anglesArray, intersection, radialPoint } from '$lib/geometry';
+	import { anglesArray, intersection, phi, radialPoint } from '$lib/geometry';
 
 	const id = 'QUASICRYSTAL';
 	const size = 2 ** 10;
 	const angles = anglesArray(10);
-	const r = size / 5.5;
+	const r = size / 2.75;
 	const lineArray: Line[] = [
 		...angles.map((a, i) => [radialPoint(a, r), radialPoint(angles[(i + 2) % 10], r)] as Line),
 		...angles.map((a, i) => [radialPoint(a, r), radialPoint(angles[(i + 4) % 10], r)] as Line)
@@ -30,11 +30,11 @@
 		lineArray.push([radialPoint(a + 18, r2), radialPoint(angles[(i + 3) % 10] + 18, r2)]);
 	});
 
-	// angles.forEach((a, i) => {
-	// 	lineArray.push([radialPoint(a, r), radialPoint(angles[(i + 3) % 10], r)]);
-	// 	lineArray.push([radialPoint(a + 18, r1), radialPoint(angles[(i + 4) % 10] + 18, r1)]);
-	// 	lineArray.push([radialPoint(a + 18, r2), radialPoint(angles[(i + 4) % 10] + 18, r2)]);
-	// });
+	angles.forEach((a, i) => {
+		// lineArray.push([radialPoint(a, r), radialPoint(angles[(i + 3) % 10], r)]);
+		// lineArray.push([radialPoint(a + 18, r1), radialPoint(angles[(i + 4) % 10] + 18, r1)]);
+		// lineArray.push([radialPoint(a + 18, r2), radialPoint(angles[(i + 4) % 10] + 18, r2)]);
+	});
 
 	const rawIntersections: Point[] = [];
 	lineArray.forEach((l, i) => {
@@ -53,36 +53,37 @@
 	roundedIntersections.forEach((intersection) => {
 		const i = JSON.stringify(intersection);
 		if (!intersectionsWithMagnitudeMap.get(i)) {
-			intersectionsWithMagnitudeMap.set(i, 2);
+			intersectionsWithMagnitudeMap.set(i, 5);
 		} else {
-			intersectionsWithMagnitudeMap.set(i, intersectionsWithMagnitudeMap.get(i) + 0.25);
+			intersectionsWithMagnitudeMap.set(i, intersectionsWithMagnitudeMap.get(i) + phi);
 		}
 	});
 	const circles: Circle[] = [...intersectionsWithMagnitudeMap].map((e) => {
 		const c = JSON.parse(e[0]);
 		return { x: c.x, y: c.y, r: e[1] };
 	});
+	const circleRadii = new Set([...circles.map((c) => c.r).sort((a, b) => a - b)]);
+	const colors = new Map([...circleRadii].map((r, i) => [r, (270 / circleRadii.size) * i + 90]));
 </script>
 
 <DopplerSvg {id} {size}>
 	<defs>
 		<filter id="QUASICRYSTAL-glow">
-			<feGaussianBlur in="SourceGraphic" stdDeviation="2" result="n" />
+			<feGaussianBlur in="SourceGraphic" stdDeviation="1" result="o" />
 			<feMorphology in="SourceGraphic" operator="dilate" radius="1" />
 			<feGaussianBlur stdDeviation="8" />
 			<feMerge>
 				<feMergeNode />
-				<feMergeNode in="n" />
-				<feMergeNode in="SourceGraphic" />
+				<feMergeNode in="o" />
 			</feMerge>
 		</filter>
 	</defs>
-	<Background {size} fill="oklch(0.2 0 0)" />
+	<Background {size} fill="oklch(0 50% 300)" />
 	<g transform="rotate(0)">
-		<LineWithLegend {lineArray} style={`stroke:oklch(0.5 25% 240)`} />
 		<g filter="url(#QUASICRYSTAL-glow)">
+			<LineWithLegend {lineArray} style={`stroke:oklch(0.25 50% 300)`} />
 			{#each circles as c}
-				<circle r={c.r} cx={c.x} cy={c.y} style={`fill:white;`} />
+				<circle r={c.r} cx={c.x} cy={c.y} style={`fill:oklch(0.75 75% ${colors.get(c.r)});`} />
 			{/each}
 		</g>
 	</g>
