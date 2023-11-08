20d6495d35f99f554ad76ea924e38d2a57b6582c
David Rose <doppler@gmail.com>
2023-11-08 06:36:02 -0600

    change 100tomatoes to starmarbles

--- src/routes/drawings/100tomatoes/+page.svelte
+++ src/routes/drawings/100tomatoes/+page.svelte
@@ -9,7 +9,7 @@
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
-	import { Phi, anglesArray, arrayMap, phi, radialPoint } from '$lib/geometry';
+	import { Phi, anglesArray, arrayMap, pentagram, phi, radialPoint } from '$lib/geometry';
 
 	const id = '100TOMATOES';
 	const size = 2 ** 10;
@@ -18,29 +18,46 @@
 	const radii = arrayMap(10, (n) => ro * phi ** n);
 	const circles: Circle[] = [
 		...radii
+			.reverse()
 			.map((r, i) =>
-				angles.map((a) => ({ r, ...radialPoint(i % 2 === 0 ? a : a + 18, r * Phi ** 3 - r) }))
+				angles.map((a) => ({ r, ...radialPoint(i % 2 === 0 ? a : a + 0, r * Phi ** 3 - r) }))
+			)
+			.flat()
+	];
+	const stars = [
+		...radii
+			.map((r) =>
+				angles.map((a, i) =>
+					pentagram(r, {
+						rotate: i % 2 === 0 ? 180 : 0,
+						center: radialPoint(i % 2 === 0 ? a : a + 0, r * Phi ** 3 - r)
+					})
+				)
 			)
 			.flat()
 	];
 </script>
 
 <DopplerSvg {id} {size}>
-	<radialGradient id="100TOMATOES-bg" r="0.5">
-		<stop offset="0%" stop-color="oklch(1 100% 150)" />
-		<stop offset="100%" stop-color="oklch(0 100% 150)" />
+	<radialGradient id="100TOMATOES-bg" r="0.7">
+		<stop offset="0%" stop-color="oklch(0 0 0)" />
+		<stop offset="100%" stop-color="oklch(1 0 0)" />
 	</radialGradient>
 	<radialGradient id="100TOMATOES-rg" r="0.5">
-		<stop offset="0%" stop-color="oklch(1 100% 90)" />
-		<stop offset="100%" stop-color="oklch(0 100% 0)" />
+		<stop offset="0%" stop-color="oklch(1 0 0)" />
+		<stop offset="100%" stop-color="oklch(0 10% 300)" />
 	</radialGradient>
 	<Background {size} fill="url(#100TOMATOES-bg)" />
-	{#each circles as c}
+	{#each circles as c, i}
 		<circle
 			r={c.r}
 			cx={c.x}
 			cy={c.y}
-			style={`fill:url(#100TOMATOES-rg);stroke:oklch(0 0 0);stroke-width:2;`}
+			style={`fill:url(#100TOMATOES-rg);stroke:oklch(0 0 0);stroke-width:1;`}
+		/>
+		<path
+			d={stars[i]}
+			style={`fill:oklch(0 0 0 / 0.125);fill-rule:evenodd;stroke:oklch(0.5 0 0);stroke-width:2`}
 		/>
 	{/each}
 </DopplerSvg>
