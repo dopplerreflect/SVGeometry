bc31e23e3a82d9071eb513bce149c8e5776f732e
David Rose <doppler@gmail.com>
2023-11-11 10:08:34 -0600

    draw lines

--- src/routes/drawings/number-mandala/+page.svelte
+++ src/routes/drawings/number-mandala/+page.svelte
@@ -9,7 +9,8 @@
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
-	import { anglesArray, arrayMap, phi, polygon, polygonPath, radialPoint } from '$lib/geometry';
+	import LineWithLegend from '$lib/components/LineWithLegend.svelte';
+	import { anglesArray, arrayMap, phi, polygonPath, radialPoint } from '$lib/geometry';
 
 	const id = 'TEMPLATE';
 	const size = 2 ** 10;
@@ -17,40 +18,204 @@
 	const angles = anglesArray(d);
 	const r = size * 0.25;
 	const radii = arrayMap(3, (n) => r * phi ** n);
-	const paths = [
-		...radii.map((r) => polygonPath(d, r, { rotate: 0 })),
-		...radii
-			.map((r) =>
-				angles.map((a) => polygonPath(d, r, { center: radialPoint(a, radii[0]), rotate: 0 }))
-			)
-			.flat()
-	];
 	const circles = [
 		...radii.map((r) => ({ r, x: 0, y: 0 })),
 		...radii.map((r) => angles.map((a) => ({ r, ...radialPoint(a, radii[0]) }))).flat()
 	];
+	const lineArray: Line[] = [
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(a, radii[0], { center: radialPoint(a, radii[0]) }),
+					radialPoint(angles[(i + 1) % d], radii[0], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 1) % d], radii[0], { center: radialPoint(a, radii[0]) }),
+					radialPoint(angles[(i + 2) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 0) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 1) % d], radii[0], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 9) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 0) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 9) % d], radii[0], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 0) % d], radii[0], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 0) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 1) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 1) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 2) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 0) % d], radii[0], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 0) % d], radii[2], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 2) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 3) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 0) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 9) % d], radii[1], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 0) % d], radii[1], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 9) % d], radii[1], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 9) % d], radii[0], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 6) % d], radii[0], {
+						center: radialPoint(angles[(i + 3) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 8) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 9) % d], radii[1], {
+						center: radialPoint(angles[(i + 2) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 7) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 8) % d], radii[2], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 3) % d], radii[2], {
+						center: radialPoint(angles[(i + 0) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 4) % d], radii[2], {
+						center: radialPoint(angles[(i + 0) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 4) % d], radii[2], {
+						center: radialPoint(angles[(i + 0) % d], radii[0])
+					}),
+					radialPoint(angles[(i + 9) % d], radii[2], {
+						center: radialPoint(angles[(i + 3) % d], radii[0])
+					})
+				] as Line
+		),
+		...angles.map(
+			(a, i) =>
+				[
+					radialPoint(angles[(i + 8) % d], radii[1], {
+						center: radialPoint(angles[(i + 1) % d], radii[0])
+					}),
+					{ x: 0, y: 0 }
+				] as Line
+		)
+	];
 </script>
 
 <DopplerSvg {id} {size}>
-	<defs>
-		<style>
-			svg#TEMPLATE {
-				& path:not(.Background),
-				polygon,
-				line,
-				circle {
-					stroke: white;
-					fill: none;
-				}
-				& circle {
-					stroke: oklch(0.5 0 0);
-				}
-			}
-		</style>
-	</defs>
 	<Background {size} fill="oklch(0.2 0 0)" />
 	{#each circles as c}
-		<circle r={c.r} cx={c.x} cy={c.y} />
-		<path d={polygonPath(d, c.r, { center: { x: c.x, y: c.y } })} />
+		<circle style="fill:none;stroke:oklch(0.5 0 0);" r={c.r} cx={c.x} cy={c.y} />
+		<!-- <path
+			style="fill:none;stroke:oklch(0.75 0 0)"
+			d={polygonPath(d, c.r, { center: { x: c.x, y: c.y } })}
+		/> -->
 	{/each}
+	<LineWithLegend showLegend {lineArray} style="stroke:oklch(0.75 0 0)" />
 </DopplerSvg>
