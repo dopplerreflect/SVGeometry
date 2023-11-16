763be54dfa538110fc312bfd77f5de42bc0b4692
David Rose <doppler@gmail.com>
2023-11-16 11:59:28 -0600

    colors and shadows and stuff

--- src/routes/drawings/phylo/+page.svelte
+++ src/routes/drawings/phylo/+page.svelte
@@ -33,41 +33,60 @@
 				& .p {
 					filter: url(#PHYLO-shrink);
 				}
+				& .c {
+					filter: url(#PHYLO-shadow);
+				}
 			}
 		</style>
 		<filter id="PHYLO-shrink">
 			<feMorphology operator="erode" radius="2" result="erode" />
 			<feMorphology in="SourceAlpha" operator="erode" radius="1" result="erode1" />
-			<feGaussianBlur stdDeviation="1" result="blur" />
+			<feFlood flood-color="oklch(1 0 0 / 0.25)" />
+			<feComposite operator="in" in2="erode1" result="border" />
+			<feGaussianBlur in="erode1" stdDeviation="3" result="blur" />
 			<feFlood flood-color="oklch(0 0 0)" />
 			<feComposite operator="in" in2="blur" />
-			<feOffset dy="2" />
+			<feOffset dy="4" />
 			<feMerge>
 				<feMergeNode />
+				<feMergeNode in="border" />
 				<feMergeNode in="erode" />
 			</feMerge>
 		</filter>
+		<filter id="PHYLO-shadow" x="-50%" y="-50%" width="200%" height="200%">
+			<feFlood flood-color="oklch(0 0 0)" />
+			<feComposite operator="in" in2="SourceAlpha" />
+			<feGaussianBlur stdDeviation="5" />
+			<feOffset dy="2" />
+			<feMerge>
+				<feMergeNode />
+				<feMergeNode in="SourceGraphic" />
+			</feMerge>
+		</filter>
 		<radialGradient id="PHYLO-rg">
-			<stop offset="0%" stop-color="oklch(1 50% 90)" />
-			<stop offset="100%" stop-color="oklch(0.5 50% 240)" />
+			<stop offset="0%" stop-color="oklch(1 100% 150)" />
+			<stop offset="100%" stop-color="oklch(0.25 100% 150)" />
 		</radialGradient>
 	</defs>
-	<Background {size} fill="oklch(0 0 0)" />
 	<Background size={size * Math.sqrt(2)} fill="url(#PHYLO-rg)" />
-	{#each paths as d, i}
-		<path
-			class="p"
-			{d}
-			style={`fill:oklch(${i % 2 === 0 ? 0.7 : 0.85} 100% ${90 + (180 / paths.length) * i})`}
-		/>
-	{/each}
-	{#each points as p, i}
-		<circle
-			class="p"
-			r={8}
-			cx={p.x}
-			cy={p.y}
-			style={`fill:oklch(${i % 3 === 1 ? 0.5 : 0.65} 100% ${90 + (180 / points.length) * i})`}
-		/>
-	{/each}
+	<g id="figure">
+		{#each paths as d, i}
+			<path
+				class="p"
+				{d}
+				style={`fill:oklch(${i % 2 === 0 ? 0.5 : 0.75} 100% ${30 + (270 / paths.length) * i})`}
+			/>
+		{/each}
+		{#each points as p, i}
+			<circle
+				class="c"
+				r={3 + (5 / points.length) * i}
+				cx={p.x}
+				cy={p.y}
+				style={`stroke:oklch(0 0 0 / 0.5);fill:oklch(${i % 3 === 1 ? 0.5 : 1} 75% ${
+					120 + (270 / points.length) * i
+				} / 1)`}
+			/>
+		{/each}
+	</g>
 </DopplerSvg>
