cf2d6e66f3bb107409fdc0804f8b1f63205a7714
David Rose <doppler@gmail.com>
2023-12-15 15:33:20 -0600

    update icircles

--- src/routes/drawings/icircles/+page.svelte
+++ src/routes/drawings/icircles/+page.svelte
@@ -11,7 +11,7 @@
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
 	import { anglesArray, multiCircleIntersections, radialPoint } from '$lib/geometry';
 
-	const id = 'TEMPLATE';
+	const id = 'ICIRCLES';
 	const size = 2 ** 10;
 	const r = size * 0.24;
 	const angles = anglesArray(60);
@@ -19,11 +19,19 @@
 	const ci = multiCircleIntersections(circles);
 </script>
 
-<DopplerSvg {id} {size}>
+<DopplerSvg {id} {size} logoProps={{ stroke: 'oklch(0.25 100% 240)' }}>
 	<defs>
-		<filter id="TEMPLATE-glow">
+		<filter id="ICIRCLES-glow">
 			<feMorphology operator="dilate" radius="1" />
-			<feGaussianBlur stdDeviation="8 3" />
+			<feGaussianBlur stdDeviation="13 3" />
+			<feMerge>
+				<feMergeNode in="SourceGraphic" />
+				<feMergeNode />
+			</feMerge>
+		</filter>
+		<filter id="ICIRCLES-glow2">
+			<feMorphology operator="dilate" radius="3" />
+			<feGaussianBlur stdDeviation="0 13" />
 			<feMerge>
 				<feMergeNode in="SourceGraphic" />
 				<feMergeNode />
@@ -31,16 +39,20 @@
 		</filter>
 	</defs>
 	<Background {size} fill="oklch(0 0 0)" />
-	{#each circles as c}
-		<circle r={c.r} cx={c.x} cy={c.y} style={`stroke:oklch(0.5 100% 300);fill:none;`} />
-	{/each}
-	<g filter="url(#TEMPLATE-glow)">
+	<g filter="url(#ICIRCLES-glow2)">
+		{#each circles as c}
+			<circle r={c.r} cx={c.x} cy={c.y} style={`stroke:oklch(0.25 100% 270);fill:none;`} />
+		{/each}
+	</g>
+	<g filter="url(#ICIRCLES-glow)">
 		{#each ci as p, i}
 			<circle
 				r={3}
 				cx={p.x}
 				cy={p.y}
-				style={`stroke:oklch(0 0 0);fill:oklch(0.99 100% ${(1440 / circles.length) * i})`}
+				style={`stroke:oklch(0.5 100% ${((360 ** 2 / ci.length) * i) % 360});fill:oklch(0.99 50% ${
+					((360 ** 2 / ci.length) * i) % 360
+				})`}
 			/>
 		{/each}
 	</g>
