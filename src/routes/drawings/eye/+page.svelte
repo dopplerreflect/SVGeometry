eeb21e68b308f50a57677c0af9c51456f509e9e0
David Rose <doppler@gmail.com>
2023-12-23 07:49:16 -0600

    tweek eye

--- src/routes/drawings/eye/+page.svelte
+++ src/routes/drawings/eye/+page.svelte
@@ -1,8 +1,8 @@
 <script context="module" lang="ts">
 	export let metadata: DrawingMetadata = {
 		tags: [],
-		created_at: new Date(),
-		updated_at: new Date()
+		created_at: new Date(2023, 11, 22),
+		updated_at: new Date(2023, 11, 22)
 	};
 </script>
 
@@ -33,20 +33,23 @@
 	<defs>
 		<style>
 			svg#EYE {
+				--lightness: 0.99;
+				--chroma: 50%;
+				--hue: 90;
 				& circle {
-					stroke: oklch(0.99 100% 90);
+					stroke: oklch(calc(var(--lightness) - 0.33) var(--chroma) var(--hue));
 					stroke-width: 3;
 					fill: none;
 				}
 				& circle.dim {
-					stroke: oklch(0.33 100% 330);
+					stroke: oklch(0.33 var(--chroma) calc(var(--hue) + 120));
 					stroke-width: 2;
 				}
 			}
 		</style>
 		<radialGradient id="EYE-rg">
-			<stop offset="0%" stop-color="oklch(0.1 100% 210)" />
-			<stop offset="100%" stop-color="oklch(0.5 100% 210)" />
+			<stop offset="0%" stop-color="oklch(0.1 var(--chroma) calc(var(--hue) + 120))" />
+			<stop offset="100%" stop-color="oklch(0.5 var(--chroma) calc(var(--hue) + 120))" />
 		</radialGradient>
 		<mask id="EYE-mask">
 			<radialGradient id="EYE-mask-rg">
@@ -66,32 +69,43 @@
 				style="stroke:white;stroke-width:3;fill:white;"
 			/>
 		</mask>
+		<filter id="EYE-line-blur">
+			<feGaussianBlur stdDeviation="8" />
+			<feMerge>
+				<feMergeNode />
+				<feMergeNode in="SourceGraphic" />
+			</feMerge>
+		</filter>
 	</defs>
-	<Background {size} fill="oklch(0.2 0 0)" />
+	<Background {size} fill="oklch(0.0125 var(--chroma) calc(var(--hue) - 120))" />
 	<g mask="url(#EYE-mask">
 		<circle {r} cy={-r / 2} style="fill:url(#EYE-rg)" />
-		{#each dimRadii as cr}
-			<circle class="dim" r={cr} cy={-r / 2} />
-		{/each}
-		{#each circles as c}
-			<circle r={c.r} cx={c.x} cy={c.y} />
-		{/each}
-		{#each angles.slice(17, 44) as a}
-			<path
-				d={`M0 ${-r / 2} ${radialPointString(a, radii[2], { center: { x: 0, y: -r / 2 } })}`}
-				style="stroke:oklch(0.99 100% 90);stroke-width:1;"
-			/>
-		{/each}
+		<g id="dimcircles" filter="url(#EYE-line-blur)">
+			{#each dimRadii as cr}
+				<circle class="dim" r={cr} cy={-r / 2} />
+			{/each}
+		</g>
+		<g id="golden-circles" filter="url(#EYE-line-blur)">
+			{#each circles as c}
+				<circle r={c.r} cx={c.x} cy={c.y} />
+			{/each}
+		</g>
+		<g id="rays" filter="url(#EYE-line-blur)">
+			{#each angles.slice(17, 44) as a}
+				<path
+					d={`M0 ${-r / 2} ${radialPointString(a, radii[2], { center: { x: 0, y: -r / 2 } })}`}
+					style="stroke:oklch(calc(var(--lightness) - 0.33) var(--chroma) var(--hue));stroke-width:1;"
+				/>
+			{/each}
 
-		{#each angles.slice(18, 43) as a}
-			<path
-				d={`M${radialPointString(a, radii[2], { center: { x: 0, y: -r / 2 } })} ${radialPointString(
-					a,
-					radii[1],
-					{ center: { x: 0, y: -r / 2 } }
-				)}`}
-				style="stroke:oklch(0.99 100% 90);stroke-width:3;"
-			/>
-		{/each}
+			{#each angles.slice(18, 43) as a}
+				<path
+					d={`M${radialPointString(a, radii[2], {
+						center: { x: 0, y: -r / 2 }
+					})} ${radialPointString(a, radii[1], { center: { x: 0, y: -r / 2 } })}`}
+					style="stroke:oklch(calc(var(--lightness) - 0.33) var(--chroma) var(--hue));stroke-width:3;"
+				/>
+			{/each}
+		</g>
 	</g>
 </DopplerSvg>
