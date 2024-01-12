65bc22259b7b522473871c619f1dc62e99ca2c91
David Rose <doppler@gmail.com>
2024-01-11 21:22:23 -0600

    globe

--- src/routes/drawings/globe/+page.svelte
+++ src/routes/drawings/globe/+page.svelte
@@ -16,7 +16,7 @@
 	const angleCount = 55;
 	const r = size * 0.455;
 	const angles = anglesArray(angleCount);
-	const hemiPoints: Point[] = angles.slice(1, angleCount / 2).map((a, i) => ({
+	const hemiPoints: Point[] = angles.slice(1, Math.ceil(angleCount / 2)).map((a, i) => ({
 		x: r * Math.cos(a * (Math.PI / 180)),
 		y: r * Math.sin(a * (Math.PI / 180))
 	}));
@@ -29,15 +29,31 @@
 </script>
 
 <DopplerSvg {id} {size}>
+	<defs>
+		<filter id="GLOBE-blur">
+			<feMorphology operator="dilate" radius="1" result="dilate" />
+			<feGaussianBlur stdDeviation="0 16" result="v" />
+			<feGaussianBlur in="dilate" stdDeviation="16 0" result="h" />
+			<feMerge>
+				<feMergeNode in="v" />
+				<feMergeNode in="h" />
+				<feMergeNode in="SourceGraphic" />
+			</feMerge>
+		</filter>
+	</defs>
 	<Background {size} fill="oklch(0.0 0 0)" />
-	{#each ellipses as ellipsePoints, ei}
-		{#each ellipsePoints as p, i}
-			<circle
-				r={2}
-				cx={p.x}
-				cy={p.y}
-				style={`fill:oklch(0.99 100% ${ei * (360 / (angleCount / 2)) + i * (360 / angleCount)})`}
-			/>
+	<g filter="url(#GLOBE-blur)" transform="rotate(23.5)">
+		{#each ellipses as ellipsePoints, ei}
+			{#each ellipsePoints as p, i}
+				<circle
+					r={2 + Math.abs(angleCount / 2 - i) * 0.0625}
+					cx={p.x}
+					cy={p.y}
+					style={`fill:oklch(0.99 100% ${
+						270 + ei * (360 / (angleCount / 2)) + i * (360 / angleCount)
+					})`}
+				/>
+			{/each}
 		{/each}
-	{/each}
+	</g>
 </DopplerSvg>
